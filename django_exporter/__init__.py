8d79505a6a3cd64b31b7f4786031364cde9b9e8e
Stephan Jaekel <steph@rdev.info>
2018-11-27 13:48:08 +0100

    Fix import problems of __version__ in setup.

--- django_exporter/__init__.py
+++ django_exporter/__init__.py
@@ -1,4 +1,8 @@
-from .exporter import Exporter, action_export_factory  # noqa
+try:
+    from .exporter import Exporter, action_export_factory  # noqa
+except ImportError:
+    # Keep the import lazy to avoid problems when the setup.py loads the version.
+    pass
 
 
 __version__ = '0.0.1'
